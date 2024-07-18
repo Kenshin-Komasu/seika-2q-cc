6008cb24f067bddd5677d1ebacda44910cf1ac23
kenshin.komasu <k221t065@st.kyoto-seika.ac.jp>
2024-07-18 17:36:53 +0900

    課題１

--- src/index.js
+++ src/index.js
@@ -1,52 +1,74 @@
-// import Swiper bundle with all modules installed
-import Swiper from 'swiper/bundle';
+import JustValidate from 'just-validate';
 
-// import styles bundle
-import 'swiper/css/bundle';
-
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper.padination',
-    clickable: true,
-
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-});
-
-//swiper.on - イベントの取得
-//ここに書く
-
-//表示するテキストを準備
-//const currentSliderIndex = swiper.realIndex;
-//console.log(currentSliderIndex)
-//const text ='スライドを表示しています'
-//console.log(text);
-//準備したテキストをHTMLに入れる
-function updatePanelNumber() {
-  var currentIndex = swiper.realIndex + 1;
-  console.log(currentIndex)
-
-  // var totalSlides = swiper.slides.length;
-  // var currentSlideText = swiper.slides[swiper.realIndex].textContent.trim();
-  document.getElementById('swiper-index').textContent = 'スライド' + currentIndex + 'を表示しています';
-}
-updatePanelNumber();
-
-swiper.on('slideChange', function () {
-  updatePanelNumber();
-})
+const validator = new JustValidate('#basic_form');
+validator
+  .addField('#basic_name', [
+    {
+      rule: 'required',
+      errorMessage: '必須入力項目です。',
+    },
+    {
+      rule: 'minLength',
+      value: 3,
+      errorMessage: '3文字以上で入力してください。',
+    },
+    {
+      rule: 'maxLength',
+      value: 15,
+      errorMessage: '最大入力文字数は15文字です。',
+    },
+  ])
+  .addField('#basic_email', [
+    {
+      rule: 'required',
+      errorMessage: '必須入力項目です。',
+    },
+    {
+      rule: 'email',
+      errorMessage: '形式が正しくありません。',
+    },
+  ])
+  .addField('#basic_password', [
+    {
+      rule: 'required',
+      errorMessage: '必須入力項目です。',
+    },
+    {
+      rule: 'password',
+      errorMessage: 'パスワードは8文字以上で入力してください。その際に数字を1文字以上含める必要があります。',
+    },
+  ])
+  .addField('#basic_age', [
+    {
+      rule: 'required',
+      errorMessage: '必須入力項目です。',
+    },
+    {
+      rule: 'number',
+      errorMessage: '数字で入力してください。',
+    },
+    {
+      rule: 'minNumber',
+      value: 18,
+      errorMessage: '18以上の数字を入力してください。',
+    },
+    {
+      rule: 'maxNumber',
+      value: 150,
+      errorMessage: '18以上の数字を入力してください。',
+    },
+  ])
+  .addField('#basic_address', [
+    {
+      rule: 'required',
+      errorMessage: '必須入力項目です。',
+    },
+  ])
+  .onSuccess(function(event){
+    let formData = new FormData(event.target);
+    console.log(formData.get("name"));
+    console.log(formData.get("email"));
+    console.log(formData.get("password"));
+    console.log(formData.get("age"));
+    console.log(formData.get("address"));
+  })
